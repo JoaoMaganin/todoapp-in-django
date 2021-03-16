005ec5c143dcf9dc76f195197f7acc6a8d936ece
JoaoMaganin <joaovitormontemor138@gmail.com>
2021-03-16 18:14:43 -0300

    Criando os arquivos models que vão para o banco de dados

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Tarefa(models.Model):
+    titulo = models.CharField('Título', max_length=20)
+    completo = models.BooleanField('Completo?', default=False)
+    data_criado = models.DateTimeField('Data de criação', auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
