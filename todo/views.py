d90179e18e535db74763df51993d683f16dbcd9b
JoaoMaganin <joaovitormontemor138@gmail.com>
2021-03-16 18:27:47 -0300

    Adicionando Tarefas ao painel de admin, adicionando inputs de criar tarefas e tarefas no template

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Tarefa
+
+from .forms import *
 
 
 def index(request):
-    return render(request, 'index.html')
+    tarefas = Tarefa.objects.all()
+
+    form = TarefaForm()
+
+    context = {
+        'tarefas': tarefas,
+        'form': form,
+    }
+
+    return render(request, 'index.html', context)
