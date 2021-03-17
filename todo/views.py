8fd1d8fa10e126f0c88639ee23f6e12d4e3a18f4
JoaoMaganin <joaovitormontemor138@gmail.com>
2021-03-17 18:06:57 -0300

    Aplicando funcionalidade de criar itens

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tarefa
 
 from .forms import *
@@ -8,6 +8,12 @@ def index(request):
     tarefas = Tarefa.objects.all()
 
     form = TarefaForm()
+
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
 
     context = {
         'tarefas': tarefas,
